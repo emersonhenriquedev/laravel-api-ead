8c307318a373b41577055f191717c217a9e2dbdb
emersonhenriquedev <emerson.hm@outlook.com>
2022-05-10 20:43:53 -0300

    Add Module and Lesson api

--- app/Http/Resources/LessonResource.php
+++ app/Http/Resources/LessonResource.php
@@ -4,7 +4,7 @@ namespace App\Http\Resources;
 
 use Illuminate\Http\Resources\Json\JsonResource;
 
-class ModuleResource extends JsonResource
+class LessonResource extends JsonResource
 {
     /**
      * Transform the resource into an array.
@@ -15,8 +15,11 @@ class ModuleResource extends JsonResource
     public function toArray($request)
     {
         return [
+            'id' => $this->id,
             'name' => ucwords(strtolower($this->name)),
-            'id' => $this->id
+            'description' => $this->description,
+            'url' => $this->url,
+            'video' => $this->video,
         ];
     }
 }
