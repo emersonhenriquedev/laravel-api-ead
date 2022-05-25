4a83cd55b99d9c1fe0de1a4156df39a95c5778b2
emersonhenriquedev <emerson.hm@outlook.com>
2022-05-25 11:02:00 -0300

    Authentication api

--- routes/api.php
+++ routes/api.php
@@ -1,12 +1,15 @@
 <?php
 
-use App\Http\Controllers\Api\ {
+use App\Http\Controllers\Api\Auth\AuthController;
+
+use App\Http\Controllers\Api\{
     CourseController,
     ModuleController,
     LessonController,
     SupportController,
     ReplySupportController
 };
+
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -21,18 +24,24 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/courses', [CourseController::class, 'index']);
-Route::get('/courses/{id}', [CourseController::class, 'show']);
+Route::post('/auth', [AuthController::class, 'auth']);
+Route::post('/logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
+Route::get('/me', [AuthController::class, 'me'])->middleware('auth:sanctum');
 
-Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
+Route::middleware(['auth:sanctum'])->group(function () {
+    Route::get('/courses', [CourseController::class, 'index']);
+    Route::get('/courses/{id}', [CourseController::class, 'show']);
 
-Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
-Route::get('/lessons/{id}', [LessonController::class, 'show']);
+    Route::get('/courses/{id}/modules', [ModuleController::class, 'index']);
 
-Route::get('/my-supports', [SupportController::class, 'mySupports']);
-Route::get('/supports', [SupportController::class, 'index']);
-Route::post('/supports', [SupportController::class, 'store']);
-Route::post('/replies', [ReplySupportController::class, 'createReply']);
+    Route::get('/modules/{id}/lessons', [LessonController::class, 'index']);
+    Route::get('/lessons/{id}', [LessonController::class, 'show']);
+
+    Route::get('/my-supports', [SupportController::class, 'mySupports']);
+    Route::get('/supports', [SupportController::class, 'index']);
+    Route::post('/supports', [SupportController::class, 'store']);
+    Route::post('/replies', [ReplySupportController::class, 'createReply']);
+});
 
 Route::get('/', function () {
     return response()->json([
